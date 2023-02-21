b5b2eb41c86ce5e58b2984ee2a151c3be4e9e097
Babs Doodkorte <babsdoodkorte@gmail.com>
2023-02-21 16:07:03 +0000

    Store Inventory

---
+++ ItemType.cs
@@ -0,0 +1,12 @@
+public class ItemType
+{
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+
+    public ItemType(string name, int quantity)
+    {
+        Name = name;
+        Quantity = quantity;
+    }
+
+}
